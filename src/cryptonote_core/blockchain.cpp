73b10b7b67f249d8684424e911242f2471e6df2c
pmitchev <support@forknote.net>
2018-01-30 08:16:38 +0200

    Testnet HF points added

--- src/cryptonote_core/blockchain.cpp
+++ src/cryptonote_core/blockchain.cpp
@@ -92,7 +92,7 @@ static const struct {
 { 1, 1, 0, 1341378000 }
 };
 
-static const uint64_t mainnet_hard_fork_version_1_till = -1;
+static const uint64_t mainnet_hard_fork_version_1_till = ((uint64_t)(-1));
 
 static const struct {
   uint8_t version;
@@ -100,10 +100,10 @@ static const struct {
   uint8_t threshold;
   time_t time;
 } testnet_hard_forks[] = {
-
+{ 1, 1, 0, 1341378000 }
 };
 
-static const uint64_t testnet_hard_fork_version_1_till = 1;
+static const uint64_t testnet_hard_fork_version_1_till = ((uint64_t)(-1));
 
 //------------------------------------------------------------------
 Blockchain::Blockchain(tx_memory_pool& tx_pool) :
